9982f8b4f6e4db2e59a838123ccd92af3d1dfde3
felerminoali <felerminoali@gmail.com>
2017-07-25 14:14:33 +0200

    First commit

---
+++ src/main/java/mz/com/osoma/rle/adaptivehuffman/AHuffman.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mz.com.osoma.rle.adaptivehuffman;
+import mz.com.osoma.huffman.HuffmanTree;
+
+/**
+ *
+ * @author feler
+ */
+public class AHuffman {
+    private HuffmanTree root;
+    
+    
+    
+}
